f645fee16c078bb715237c6115525a5ae4d14987
sereenaa <serenax8@gmail.com>
2020-03-17 14:19:44 +1100

    get table data printed in output

--- asx_test_scrape.py
+++ asx_test_scrape.py
@@ -3,5 +3,10 @@ from bs4 import BeautifulSoup
 
 URL = 'https://au.finance.yahoo.com/quote/%5EAXJO/history?p=%5EAXJO'
 page = requests.get(URL)
-
 soup = BeautifulSoup(page.content, 'html.parser')
+
+for row in soup.find_all('td', class_ = 'Py(10px)'): 
+    text = row.text
+    print(text)
+
+
