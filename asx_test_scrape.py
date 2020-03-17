13679adf9482b6517baf8617971439383e82d5dc
sereenaa <serenax8@gmail.com>
2020-03-17 12:05:51 +1100

    add beautiful soup

--- asx_test_scrape.py
+++ asx_test_scrape.py
@@ -1,4 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = 'https://au.finance.yahoo.com/quote/%5EAXJO/history?p=%5EAXJO'
 page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
